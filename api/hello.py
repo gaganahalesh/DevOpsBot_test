70e5f66bb8026ce3ddacbcfd00d169bf2fba2366
H Gagana <gagana.h@nokia.com>
2025-10-31 17:16:08 +0530

    Testing deploy

--- api/hello.py
+++ api/hello.py
@@ -1,6 +1,10 @@
 def handler(request):
+    # You can still log this for debugging
+    print("Hello from Python (this will go to Vercel logs)")
+    
+    # But this is what the user sees in the browser
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "application/json"},
-        "body": '{"message": "Hello, World!"}'
+        "body": '{"message": "Hello from Python!"}'
     }
