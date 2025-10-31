d4abbce309c1b1890a8fe9abb815ffaec996a3a6
H Gagana <gagana.h@nokia.com>
2025-10-31 17:37:04 +0530

    Dummy reveiw

--- api/hello.py
+++ api/hello.py
@@ -1,7 +1,7 @@
 def handler(request):
-    # This is the simplest working Vercel Python handler
     return {
         "statusCode": 200,
         "headers": {"Content-Type": "text/plain"},
         "body": "Hello from Python!"
     }
+
